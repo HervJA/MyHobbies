de2f8f4bc54a1df004c0b40db791e171d98c4815
HervJA <hjosephantoine@gmail.com>
2021-06-19 12:30:27 +0200

    With policies implemented

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Tag;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class TagController extends Controller
 {
@@ -83,6 +84,7 @@ class TagController extends Controller
      */
     public function edit(Tag $tag)
     {
+        abort_unless(Gate::allows('update',$tag),403);
         return view('tag.edit')->with(['tag'=>$tag]);
     }
 
@@ -95,6 +97,8 @@ class TagController extends Controller
      */
     public function update(Request $request, Tag $tag)
     {
+        abort_unless(Gate::allows('update',$tag),403);
+        
         $request->validate([
             'name'=>'required|min:3',
             'style'=>'required|min:5',
@@ -120,6 +124,8 @@ class TagController extends Controller
      */
     public function destroy(Tag $tag)
     {
+        abort_unless(Gate::allows('delete',$tag),403);
+        
         $oldname = $tag->name ;
         $tag->delete();
         return $this->index()->with(
